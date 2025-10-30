47d8d9b09ce36ecc7da5c77e44e1770c77901e06
thatsnotrlght <jartiste23@gmail.com>
2025-10-29 23:15:52 -0400

    Initial setup

---
+++ src/main/java/com/research_assistant/ResearchController.java
@@ -0,0 +1,23 @@
+package com.research_assistant;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/research") // Mapping all endpoints defined in Controller to this URL
+@CrossOrigin(origins = "*") // Allows accessing all endpoints in this controller from frontend
+@AllArgsConstructor
+public class ResearchController {
+	private final ResearchService researchService;
+	
+	@PostMapping("/process")
+	public ResponseEntity<String> processContent() {
+		return "";
+	}
+	
+}
---
+++ src/main/java/com/research_assistant/ResearchService.java
@@ -0,0 +1,8 @@
+package com.research_assistant;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResearchService {
+
+}
